3c26dd28ee2301bbd3f8e13fc448b3c50f83cd74
Chec98Qu <chec98qu@gmail.com>
2018-05-08 23:58:27 +1000

    new changes

--- app/src/main/java/com/example/a62645/fitness/Activity/ActivityActivity.java
+++ app/src/main/java/com/example/a62645/fitness/Activity/ActivityActivity.java
@@ -20,7 +20,7 @@ public class ActivityActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_activity);
 
         setupBottomNavigationView();
     }
--- app/src/main/java/com/example/a62645/fitness/Sleep/SleepActivity.java
+++ app/src/main/java/com/example/a62645/fitness/Sleep/SleepActivity.java
@@ -20,7 +20,7 @@ public class SleepActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_explore);
 
         setupBottomNavigationView();
     }
