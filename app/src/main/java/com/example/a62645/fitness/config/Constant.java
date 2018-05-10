7272703e4ccb8223dc84f7a23f7f4b58b591c539
Chec98Qu <chec98qu@gmail.com>
2018-05-10 13:02:37 +1000

    initial start

---
+++ app/src/main/java/com/example/a62645/fitness/config/Constant.java
@@ -0,0 +1,9 @@
+package com.example.a62645.fitness.config;
+
+
+public class Constant {
+    public static final int MSG_FROM_CLIENT=0;
+    public static final int MSG_FROM_SERVER=1;
+    public static final int REQUEST_SERVER=2;
+    public static final String DB_NAME="basepedo";
+}
